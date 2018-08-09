8ceee06e1c93025628cd55d50dcdaeacf850d265
Rafael Prado <rps4@cin.ufpe.br>
2018-08-09 20:05:36 -0300

    retestando com insertLogTest o ip #4

--- queries.js
+++ queries.js
@@ -54,6 +54,7 @@ function insertLog(ip, func, inObj, outObj) {
 function insertLogTest(req, res, next) {
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     ip = ""+ip;
+    alert(ip);
     /*var currentdate = new Date(); 
     var hourtime = currentdate.getDate() + "/"
                 + (currentdate.getMonth()+1)  + "/" 
