9bc50c302441cfa3faa77404295bb19f2d9dc8c1
Rafael Prado <rps4@cin.ufpe.br>
2018-08-09 20:12:07 -0300

    retestando com insertLogTest o ip #7

--- queries.js
+++ queries.js
@@ -64,7 +64,7 @@ function insertLogTest(req, res, next) {
     var inObj = JSON.stringify(inObj);
     var outObj = JSON.stringify(outObj);*/
     db.none("insert into log (IP) " +
-      "values('-'')")
+      "values('-')")
     .then(function () {
       res.status(200)
         .json({
