58b1a208136c5ed4faa845dcb21809f9af8780d5
Rafael Prado <rps4@cin.ufpe.br>
2018-08-09 19:08:04 -0300

    ajustes no insertLogTest para debugar

--- queries.js
+++ queries.js
@@ -28,7 +28,28 @@ function getLog(req, res, next) {
 
 function insertLogTest(req, res, next) {
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  insertLog(ip, 'getLog', '', '');
+  var currentdate = new Date(); 
+    var hourtime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    var inObj = JSON.stringify(inObj);
+    var outObj = JSON.stringify(outObj);
+    db.none('insert into log(ip, hourtime, function, inObj, outObj)' +
+      'values($1, $2, $3, $4',
+    ip, hourtime, 'logTest', '-', '-')
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Inserted on log'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
 }
 
 function insertLog(ip, func, inObj, outObj) {
