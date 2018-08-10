a0f1f4b71abd2b2d01bc7d82f4c10354ff849d56
Rafael Prado <rps4@cin.ufpe.br>
2018-08-10 00:08:33 -0300

    Aceitando solicitacoes #2

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
