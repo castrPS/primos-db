484436fd7709694a6af87e7204689e6f894f7b9a
Rafael Prado <rps4@cin.ufpe.br>
2018-08-10 00:14:37 -0300

    Aceitando solicitacoes #4

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var cors = require('cors');
 
 var app = express();
 
@@ -24,6 +25,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors({origin: 'https://colmeia-dos-primos.herokuapp.com/'}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
